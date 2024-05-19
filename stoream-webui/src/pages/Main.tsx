cab0ad0607b6da14a8613af4f7a90bc940e8f025
Muqiu Han <muqiu-han@outlook.com>
2024-05-19 22:31:27 +0800

    [webui]: add capacity to Main component and optimize some details
    Change-Id: I5f3f99276c8e0344bc63a848bbe03e0060d1cc67

--- stoream-webui/src/pages/Main.tsx
+++ stoream-webui/src/pages/Main.tsx
@@ -1,37 +1,49 @@
-import { Grid, Page, Pagination, Spacer } from "@geist-ui/core"
+import { Capacity, Card, Divider, Grid, Page, Pagination, Spacer, Tooltip, useTheme } from "@geist-ui/core"
 import UserDetails from "../components/UserDetails"
 import Header from "../components/Header"
 import Files from "../components/Files"
 import Folders from "../components/Folders"
 
 const Main = ({ hidden }) => {
-    const uri = window.location.pathname
-    const content = (uri === '/' ? <Files /> : <Folders />)
+    const theme = useTheme()
+
     return (
         <div hidden={hidden}>
-            <Page dotBackdrop width="95%" padding={0}>
-                <UserDetails />
-
-                <Spacer h={2} />
-                <Grid.Container>
-                    <Grid xs={4}>
-                        <Folders />
+            <Page width="80%">
+                <Grid.Container gap={1}>
+                    <Grid xs>
+                        <Tooltip type="success" text="Occupied: 15%, remaining: 75%" placement="right">
+                            <Capacity value={15} color={theme.palette.error} width={5} scale={1.5} />
+                        </Tooltip>
                     </Grid>
-                    <Grid xs={20}>
-                        <Grid.Container justify='center'>
-                            <Grid xs={24}>
-                                <Header />
-                            </Grid>
-                            <Spacer inline h={2} />
-                            <Grid xs={24}>
-                                {content}
-                            </Grid>
-                            <Spacer inline h={4} />
-                            <Grid xs={24} justify='center'>
-                                <Pagination count={20} initialPage={1} />
-                            </Grid>
-                        </Grid.Container>
+                    <Grid xs={5} justify="right">
+                        <UserDetails />
                     </Grid>
+                    <Grid xs={24}>
+                        <Header />
+                    </Grid>
+                    <Spacer h={2} />
+                    <Grid.Container gap={1} >
+                        <Grid>
+                            <Card shadow paddingRight={5}>
+                                <Folders />
+                            </Card>
+                        </Grid>
+                        <Spacer w={3} />
+                        <Grid xs>
+                            <Card shadow>
+                                <Grid.Container gap={1}>
+                                    <Grid xs={24}>
+                                        <Files />
+                                    </Grid>
+                                    <Spacer inline h={4} />
+                                    <Grid xs={24} justify='center'>
+                                        <Pagination count={20} initialPage={1} />
+                                    </Grid>
+                                </Grid.Container>
+                            </Card>
+                        </Grid>
+                    </Grid.Container>
                 </Grid.Container>
             </Page>
         </div>
