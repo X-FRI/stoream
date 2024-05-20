0a26e11c086439da42d2ff67962290ebaa244a14
Muqiu Han <muqiu-han@outlook.com>
2024-05-20 22:41:42 +0800

    [engine]: add file tree logic
    Change-Id: I53e1e30bca40c26bee1324b039d6d077610d1b4e

--- stoream-engine/src/main.rs
+++ stoream-engine/src/main.rs
@@ -1,4 +1,9 @@
-use std::{thread::sleep, time::Duration};
+use std::{
+    env,
+    fs::{self, Metadata},
+    thread::sleep,
+    time::Duration,
+};
 
 use axum::{
     extract::Query,
@@ -18,6 +23,17 @@ struct User {
     password: String,
 }
 
+#[derive(Serialize, Deserialize, Debug)]
+struct Path {
+    path: String,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Folder {
+    name: String,
+    path: String,
+}
+
 #[tokio::main]
 async fn main() {
     colog::init();
@@ -32,11 +48,43 @@ async fn main() {
         tokio::net::TcpListener::bind("localhost:9993")
             .await
             .unwrap(),
-        Router::new().route("/login", get(login).layer(cors)),
+        Router::new()
+            .route("/login", get(login).layer(cors.clone()))
+            .route("/path", get(path).layer(cors)),
     );
     info!("stoream engine started at http://localhost:9993");
 
     server.await.unwrap();
+}
+
+fn get_dir_list(path: String) -> Vec<Folder> {
+    fs::read_dir(path)
+        .unwrap()
+        .filter(move |file| {
+            file.as_ref()
+                .map(|file| std::fs::metadata(file.path()).unwrap())
+                .map(|metadata| metadata.is_dir())
+                .unwrap()
+        })
+        .map(|dir| dir.unwrap())
+        .map(|dir_entry| Folder {
+            name: dir_entry.file_name().to_str().unwrap().to_string(),
+            path: dir_entry.path().to_str().unwrap().to_string(),
+        })
+        .collect::<Vec<_>>()
+}
+
+async fn path(Query(path): Query<Path>) -> impl IntoResponse {
+    info!("request path {}", path.path);
+
+    (
+        StatusCode::OK,
+        Json(json!(if path.path.is_empty() {
+            get_dir_list((env::args().collect::<Vec<String>>()[1]).clone())
+        } else {
+            get_dir_list(path.path)
+        })),
+    )
 }
 
 async fn login(Query(user): Query<User>) -> impl IntoResponse {
