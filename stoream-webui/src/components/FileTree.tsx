644e54fccbb443faa9b0fbacae81e9505d425ac4
Muqiu Han <muqiu-han@outlook.com>
2024-05-21 20:24:21 +0800

    [webui]: optimize the error handler
    Change-Id: I3b84f1beb228fbdd74fed14495c523bfe9fc7695

--- stoream-webui/src/components/FileTree.tsx
+++ stoream-webui/src/components/FileTree.tsx
@@ -1,5 +1,34 @@
-import { Tree } from "@geist-ui/core"
+/// Copyright (c) 2024 The X-Files Research Institute
+/// 
+/// All rights reserved.
+/// 
+/// Redistribution and use in source and binary forms, with or without modification,
+/// are permitted provided that the following conditions are met:
+/// 
+///     * Redistributions of source code must retain the above copyright notice,
+///       this list of conditions and the following disclaimer.
+///     * Redistributions in binary form must reproduce the above copyright notice,
+///       this list of conditions and the following disclaimer in the documentation
+///       and/or other materials provided with the distribution.
+///     * Neither the name of Stoream nor the names of its contributors
+///       may be used to endorse or promote products derived from this software
+///       without specific prior written permission.
+/// 
+/// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+/// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+/// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+/// A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+/// CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+/// EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+/// PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+/// PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+/// LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+/// NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+/// SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+import { Tree, useToasts } from "@geist-ui/core"
 import { t as Folder } from "../model/FileTree.gen.tsx"
+import { FileTree as RequestFileTree } from "../model/Request.gen.tsx"
 
 const mapFolder = (folder: Folder) => {
     if (folder.sub === undefined) {
@@ -14,14 +43,24 @@ const mapFolder = (folder: Folder) => {
     }
 }
 
-const FileTree = ({content}) => {
+const loadFolder = async (path: string) => {
+    let fileTree = await RequestFileTree.request(path)
+    if (fileTree.TAG === "Ok") {
+        return fileTree._0
+    } else {
+        console.log(fileTree._0)
+    }
+}
+
+const FileTree = ({ content }) => {
     return (
         <>
-            <Tree>
+            <Tree initialExpand>
                 {mapFolder(content)}
             </Tree>
         </>
     )
 }
 
+export { loadFolder }
 export default FileTree
--- stoream-webui/src/components/LoginForm.tsx
+++ stoream-webui/src/components/LoginForm.tsx
@@ -1,34 +1,60 @@
-import { Button, Grid, Input, Spacer } from "@geist-ui/core"
+/// Copyright (c) 2024 The X-Files Research Institute
+/// 
+/// All rights reserved.
+/// 
+/// Redistribution and use in source and binary forms, with or without modification,
+/// are permitted provided that the following conditions are met:
+/// 
+///     * Redistributions of source code must retain the above copyright notice,
+///       this list of conditions and the following disclaimer.
+///     * Redistributions in binary form must reproduce the above copyright notice,
+///       this list of conditions and the following disclaimer in the documentation
+///       and/or other materials provided with the distribution.
+///     * Neither the name of Stoream nor the names of its contributors
+///       may be used to endorse or promote products derived from this software
+///       without specific prior written permission.
+/// 
+/// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+/// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+/// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+/// A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+/// CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+/// EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+/// PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+/// PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+/// LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+/// NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+/// SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+import { Button, Grid, Input, Spacer, useToasts } from "@geist-ui/core"
 import { LogIn, User as UserIcon } from "@geist-ui/icons"
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import { User as RequestUser } from "../model/Request.gen.tsx"
+import { toString } from "../model/Errors.gen.tsx"
 
-const login = async (user, setErrorMessage, setErrorMessageTitle, setErrorModalVisible, navigate) => {
+const login = async (user, setToast, navigate) => {
     await
         RequestUser
             .request(user)
             .then(result => {
                 if (result.TAG === "Error") {
-                    setErrorMessageTitle(result._0.TAG)
-                    setErrorMessage(result)
-                    setErrorModalVisible(true)
+                    setToast({ text: toString(result._0), type: "error" })
                 } else {
                     navigate("/files")
                 }
             })
             .catch(reason => {
-                setErrorMessageTitle("There was a problem during login")
-                setErrorMessage(String(reason))
-                setErrorModalVisible(true)
+                setToast({ text: String(reason), type: "error" })
             })
 }
 
-const LoginForm = ({ setErrorMessage, setErrorMessageTitle, setVisible }) => {
+const LoginForm = () => {
     const [loginButtonLoading, setLoginButtonLoading] = React.useState(false);
     const usernameRef: any = React.useRef("")
     const passwordRef: any = React.useRef("")
     const navigate = useNavigate()
+    const { setToast } = useToasts({ placement: "topRight", padding: "1em" })
 
     return (
         <>
@@ -61,9 +87,7 @@ const LoginForm = ({ setErrorMessage, setErrorMessageTitle, setVisible }) => {
                                     username: usernameRef.current.value,
                                     password: passwordRef.current.value
                                 },
-                                setErrorMessage,
-                                setErrorMessageTitle,
-                                setVisible,
+                                setToast,
                                 navigate,
                             )
                             setLoginButtonLoading(false)
