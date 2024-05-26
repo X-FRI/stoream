d1802137036441ccefed04333c3089bb6c186689
Muqiu Han <muqiu-han@outlook.com>
2024-05-26 11:56:23 +0800

    [engine]: Use FSharp.SystemTextJson instead of FSharp.Json

--- stoream-engine/src/Config/Config.fs
+++ stoream-engine/src/Config/Config.fs
@@ -1,16 +1,22 @@
 module Stoream.Engine.Config
 
 open System
-open FSharp.Json
 
+[<Text.Json.Serialization.JsonFSharpConverterAttribute>]
 type AccountConfig = { Username: String; Password: String }
+
+[<Text.Json.Serialization.JsonFSharpConverterAttribute>]
 type ServerConfig = { Hostname: String; Port: UInt16 }
+
+[<Text.Json.Serialization.JsonFSharpConverterAttribute>]
 type StorageConfig = { Root: String; Capacity: UInt32 }
 
+[<Text.Json.Serialization.JsonFSharpConverterAttribute>]
 type Config =
     { Account: AccountConfig
       Server: ServerConfig
       Storage: StorageConfig }
 
     static member public CONFIG =
-        IO.File.ReadAllText("./stoream-engine.json") |> Json.deserialize<Config>
+        IO.File.ReadAllText("./stoream-engine.json")
+        |> Text.Json.JsonSerializer.Deserialize<Config>
