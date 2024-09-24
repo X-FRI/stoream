79db6cdb74993fbfbc78c17c1c0461596ef8c2ac
Muqiu Han <muqiu-han@outlook.com>
2024-09-24 20:13:08 +0800

    [webui]: init

---
+++ webui/src/routes/+layout.svelte
@@ -0,0 +1,12 @@
+<script>
+	import Header from './Header.svelte';
+	import '../app.css';
+</script>
+
+<div class="app">
+	<Header></Header>
+
+	<main>
+		<slot></slot>
+	</main>
+</div>
---
+++ webui/src/routes/+page.svelte
@@ -0,0 +1,16 @@
+<script>
+	import Counter from './Counter.svelte';
+</script>
+
+<svelte:head>
+	<title>Home</title>
+	<meta name="description" content="Svelte demo app" />
+</svelte:head>
+
+<section>
+	<h1>
+		Stoream
+	</h1>
+
+	<Counter />
+</section>
